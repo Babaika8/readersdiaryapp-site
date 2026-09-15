aab5377eea5be40950ed8a7a6b6548c20b99eda7
Dima <dima@example.com>
2026-09-15 19:24:36 +0500

    fix: use current VK ID web application

--- social-auth-config.js
+++ social-auth-config.js
@@ -2,6 +2,6 @@
 // Secrets never belong in this file.
 window.BookDiarySocialAuth = Object.freeze({
   googleClientId: "929765526328-b13o4bkif8rh48cf9g0le1p67be5dpe8.apps.googleusercontent.com",
-  vkAppId: "54763577",
+  vkAppId: "54772894",
   vkRedirectUrl: "https://readersdiaryapp.ru/premium.html"
 });
